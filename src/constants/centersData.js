613066d2c0036b58060f5ef6e724d296c4793a95
DEBTULYA2005 <debtulya463@gmail.com>
2026-07-18 20:53:43 +0530

    second Commit

--- src/constants/centersData.js
+++ src/constants/centersData.js
@@ -1,23 +1,23 @@
 export const CENTERS = [
   {
+    id: 'durgapur',
+    name: 'Durgapur Center (Head Office)',
+    address: 'Rabindra Pally, Durgapur, West Bengal, India',
+    phone: '+91 98511 91870',
+    email: 'durgapur.orbit@gmail.com', 
+    image: 'https://images.unsplash.com/photo-1519452575417-564c1401ecc0?w=800&q=80',
+    mapEmbedUrl:
+      'https://www.google.com/maps?q=Durgapur,West+Bengal,India&output=embed',
+  },
+  {
     id: 'asansol',
-    name: 'Asansol Center (Head Office)',
+    name: 'Asansol Center',
     address: 'Orbit Educations Campus, G.T. Road, Asansol, West Bengal, India',
     phone: '+91 00000 00001',
     email: 'asansol@orbiteducations.in',
     image: 'https://images.unsplash.com/photo-1562774053-701939374585?w=800&q=80',
     mapEmbedUrl:
       'https://www.google.com/maps?q=Asansol,West+Bengal,India&output=embed',
-  },
-  {
-    id: 'durgapur',
-    name: 'Durgapur Center',
-    address: 'Rabindra Pally, Durgapur, West Bengal, India',
-    phone: '+91 00000 00002',
-    email: 'durgapur@orbiteducations.in',
-    image: 'https://images.unsplash.com/photo-1519452575417-564c1401ecc0?w=800&q=80',
-    mapEmbedUrl:
-      'https://www.google.com/maps?q=Durgapur,West+Bengal,India&output=embed',
   },
   {
     id: 'kolkata',
